3bffd95244cbb2f4b69487d83deb3380ea63084c
s1132234 <s1132234@pu.edu.tw>
2025-11-03 16:16:01 +0800

    v4_計數器

--- app/src/main/java/tw/edu/pu/o365/s1132234/MoleViewModel.kt
+++ app/src/main/java/tw/edu/pu/o365/s1132234/MoleViewModel.kt
@@ -4,14 +4,33 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MoleViewModel: ViewModel() {
 
     var counter by mutableLongStateOf(0)
-        private set  //表示只有 ViewModel 內部可以修改它
+        private set
+
+    var stay by mutableLongStateOf(0)
+        private set
 
     fun incrementCounter() {
         counter++
+    }
+
+    init {
+        startCounting()
+    }
+
+    private fun startCounting() {
+        viewModelScope.launch {
+            while (true) {
+                delay(1000L)
+                stay++
+            }
+        }
     }
 
 
